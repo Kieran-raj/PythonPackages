acf3889d1258f20b389c91e6045f163277a6636d
Kieran <kieran@rajasansir.co.uk>
2021-10-30 22:10:56 +0100

    edited packages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     description='A python package which contains modules for personal use',
     author='Kieran Rajasansir',
-    packages=find_packages(),
+    packages=find_packages(include=['expenses_data_upload', 'expenses_data_upload.helpers']),
     install_requires=[
         'pdfplumber'
     ]
